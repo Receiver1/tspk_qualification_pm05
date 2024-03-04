e59cc823dd1d55d6d5f3f71b0d6a88755f19fdc1
Aleksander Kot <receiver8@yandex.ru>
2024-03-05 03:04:17 +0400

    design 3

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,12 +6,34 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("Вы зашли!") }}
+        <div class = "grid grid-cols-2 gap-1 ">
+
+        <div class="w-full mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 shadow-sm sm:rounded-lg">
+                <div class="min-h-72 p-6 text-gray-900 dark:text-gray-100 flex flex-col justify-around">
+                    <div>
+                        <div class="text-gray-500">{{ __("Номер заявления: ") }}</div>
+                        <div>1488</div>
+                    </div>
+                    <div>
+                        <div class="text-gray-500">{{ __("Статус заявления: ") }}</div>
+                        <div>Пошёл нахуй</div>
+                    </div>
+                    <div>
+                        <div class="text-gray-500">{{ __("Номер автомобиля: ") }}</div>
+                        <div>Х715УЙ 163</div>
+                    </div>
+                    <div>
+                        <div class="text-gray-500">{{ __("Описание: ") }}</div>
+                        <div class="whitespace-pre-wrap break-words">{{
+                            __("asdasdasdas")
+                            }}</div>
+                    </div>
                 </div>
             </div>
+        </div>
+
+
         </div>
     </div>
 </x-app-layout>
